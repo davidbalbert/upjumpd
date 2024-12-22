96501b2682003930bb729775fdb5fe2d5ce0dd48
David Albert <davidbalbert@gmail.com>
2024-12-21 20:36:46 -0600

    Add virtio net device, get rid of ethernet and wlan drivers

--- run.sh
+++ run.sh
@@ -9,5 +9,7 @@ qemu-system-aarch64 \
     -serial mon:stdio \
     -drive file=buildroot/output/images/rootfs.squashfs,format=raw,if=none,id=hd0 \
     -device virtio-blk-device,drive=hd0 \
+    -netdev user,id=net0 \
+    -device virtio-net-device,netdev=net0 \
     -kernel buildroot/output/images/Image \
     -append "console=ttyAMA0 root=/dev/vda"
