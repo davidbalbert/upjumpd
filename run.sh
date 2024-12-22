3aa52f6299f545b55df9d61549f75db33d594dd6
David Albert <davidbalbert@gmail.com>
2024-12-21 20:30:28 -0600

    Boot from squashfs

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-qemu-system-aarch64 -machine virt -accel hvf -cpu host -m 256 -kernel buildroot/output/images/Image -nographic -serial mon:stdio
+qemu-system-aarch64 \
+    -machine virt \
+    -accel hvf \
+    -cpu host \
+    -m 256 \
+    -nographic \
+    -serial mon:stdio \
+    -drive file=buildroot/output/images/rootfs.squashfs,format=raw,if=none,id=hd0 \
+    -device virtio-blk-device,drive=hd0 \
+    -kernel buildroot/output/images/Image \
+    -append "console=ttyAMA0 root=/dev/vda"
